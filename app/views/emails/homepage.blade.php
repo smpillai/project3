7d92e1b706472327312f043a0e7047374352db57
smpillai <spillai01@g.harvard.edu>
2014-10-24 15:00:54 -0400

    Created the landing page

---
+++ app/views/emails/homepage.blade.php
@@ -0,0 +1,21 @@
+@extends('_master')
+
+
+@stop
+
+@section('content')
+	
+
+	<h3>Lorem Ipsum Generator</h3>
+	<p>
+		 Lorem ipsum generators are used for all the webdesigners, designers, webmasters who need to add dummy text temporarily to fill in content for their applications. To create random filler text for your applications <a href="loremgen">CLICK ME!</a>.
+	</p>
+
+	<h3>Random User Generator</h3>
+	<p>
+		To create random user data which includes names, birthdate and their profiles 
+		<a href="usergen">CLICK ME!</a>.
+	</p>
+
+
+@stop
