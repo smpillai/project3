cbc3e198d8d054d4844aa70a8ffe339049c871d4
smpillai <spillai01@g.harvard.edu>
2014-10-24 14:59:15 -0400

    Added master page

---
+++ app/views/_master.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>
+		@yield('title','Project3')
+	</title>
+	<meta charset="utf-8" />
+
+</head>
+<body>
+	<h1>Sharath's Project 3</h1>
+	
+         <div>               	
+		@yield('content')
+	</div>
+</body>
+</html>
